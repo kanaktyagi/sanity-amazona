913b56d85b62b4d8a40fbd26b3f6b55da93fb52a
kanaktyagi <tyagiknk.india@gmail.com>
2022-04-06 22:16:45 +0530

    Added the layout component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,14 @@
 import { Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout  from '../components/Layout'
 
 
 export default function Home() {
   return (
     <div>
     
-      <Head>
-        <title>Sanity Amazona</title>
-        <meta name="description" content="the ecommerce website by next and sanity" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant='h1'> Sanity Amazona
-      </Typography>
+      <Layout >List Products</Layout>
     </div>
   )
 }
