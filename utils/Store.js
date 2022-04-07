5075f380c3e38c17eb0453dec82f70a9e6162aa3
kanaktyagi <tyagiknk.india@gmail.com>
2022-04-07 21:48:03 +0530

    Added the new package and created the store for handling themes

---
+++ utils/Store.js
@@ -0,0 +1,27 @@
+import Cookies from 'js-cookie';
+import { createContext, useReducer } from 'react';
+
+export const Store = createContext();
+
+const intialState = {
+    darkMode : Cookies.get('darkMode') === 'ON' ? true : false,
+}
+
+function reducer(state,action) {
+    switch(action.type) {
+        case 'DARK_MODE_ON': 
+        return {
+            ...state, darkMode: true
+        };
+        case 'DARK_MODE_OFF' : 
+        return {
+            ...state, darkMode: false
+        }
+        default: 
+        return state
+    }
+}
+
+export function StoreProvider(props) {
+    const [state, dispatch] = useReducer(reducer, intialState)
+}
